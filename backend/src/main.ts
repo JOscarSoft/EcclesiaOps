719e9f7e06d09773a75bfa707f00c61b3995c2e7
JOscarSoft <jordy.dcna@gmail.com>
2026-05-08 14:18:28 -0400

    Change for vercel deploy

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,7 +7,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.setGlobalPrefix('api');
-  app.enableCors();
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
   const config = new DocumentBuilder()
